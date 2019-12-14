586f3dfbb93d063210ada97fbb9b63ba827b7dfe
verjeff <58576125+verjeff@users.noreply.github.com>
2019-12-14 19:24:18 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
-public class Main {
-
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-
+package javaapplication4;
+public class JavaApplication4 {
+    public static void main(String[] args) {
+        Taxi x = new Taxi("ILY143");
+        x.run ("ur");
+        x.printCurrentLocation();
+        //x:____ , y:____
+        x.book(1,1);
+        //x.run("I am leaving on a taxi");
+        System.out.println(x.reachedDestination());
+        //false
+    }
+    
 }
